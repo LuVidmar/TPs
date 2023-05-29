bef19b01b7374153f98c64258fa4ff743c672b18
Lucas Vidmar <lucas@vidmar.xyz>
2023-05-29 14:02:40 -0300

    pwm uart

---
+++ U3 - PWM UART/Core/Inc/tim.h
@@ -0,0 +1,44 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    tim.h
+  * @brief   This file contains all the function prototypes for
+  *          the tim.c file
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __TIM_H__
+#define __TIM_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Timers */
+extern TIM_HandleTypeDef htim2;
+extern TIM_HandleTypeDef htim3;
+
+/* Inicializaciones */
+void MX_TIM2_Init(void);
+void MX_TIM3_Init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __TIM_H__ */
+
